aa25cec913c9b5fa5efc8231ad59bfc06fb064da
Tim Kopka <tkopka@student.unimelb.edu.au>
2026-01-16 16:09:38 +1100

    add option to round to integers conservatively

--- src/model/model_readout.jl
+++ src/model/model_readout.jl
@@ -1,16 +1,31 @@
-function get_results(m)
+function get_results(m; conservative_rounding=false)
 
-    # Storage charging/discharging profiles
-    p_stor_charge = round.(Int,value.(m[:p_stor_charge]))
-    p_stor_discharge = round.(Int,value.(m[:p_stor_discharge]))
-    e_stor = round.(Int, value.(m[:e_stor]))
-    e_stor_ini = round.(Int, value.(m[:stor_initial_soc]))
+    if conservative_rounding
+        # Storage charging/discharging profiles
+        p_stor_charge = ceil.(Int,value.(m[:p_stor_charge]))
+        p_stor_discharge = floor.(Int,value.(m[:p_stor_discharge]))
+        e_stor = floor.(Int, value.(m[:e_stor]))
+        e_stor_ini = floor.(Int, value.(m[:stor_initial_soc]))
 
-    # Generator-Storage charging/discharging profiles
-    p_genstor_charge = round.(Int,value.(m[:p_genstor_charge]))
-    p_genstor_discharge = round.(Int,value.(m[:p_genstor_discharge]))
-    e_genstor = round.(Int,value.(m[:e_genstor]))
-    e_genstor_ini = round.(Int, value.(m[:genstor_initial_soc]))
+        # Generator-Storage charging/discharging profiles
+        p_genstor_charge = ceil.(Int,value.(m[:p_genstor_charge]))
+        p_genstor_discharge = floor.(Int,value.(m[:p_genstor_discharge]))
+        e_genstor = floor.(Int,value.(m[:e_genstor]))
+        e_genstor_ini = floor.(Int, value.(m[:genstor_initial_soc]))
+
+    else
+        # Storage charging/discharging profiles
+        p_stor_charge = round.(Int,value.(m[:p_stor_charge]))
+        p_stor_discharge = round.(Int,value.(m[:p_stor_discharge]))
+        e_stor = round.(Int, value.(m[:e_stor]))
+        e_stor_ini = round.(Int, value.(m[:stor_initial_soc]))
+
+        # Generator-Storage charging/discharging profiles
+        p_genstor_charge = round.(Int,value.(m[:p_genstor_charge]))
+        p_genstor_discharge = round.(Int,value.(m[:p_genstor_discharge]))
+        e_genstor = round.(Int,value.(m[:e_genstor]))
+        e_genstor_ini = round.(Int, value.(m[:genstor_initial_soc]))
+    end
 
     return (stor_charging=p_stor_charge,
         stor_discharging=p_stor_discharge,
