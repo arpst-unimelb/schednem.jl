c108a5890ffa1fb0178f1fb6c15bf5f0225b7017
Tim Kopka <tkopka@student.unimelb.edu.au>
2026-02-12 16:17:42 +1100

    Bug fix: Get results if not all technologies included

--- src/analysis/getResults.jl
+++ src/analysis/getResults.jl
@@ -1,31 +1,44 @@
 function get_results(m; conservative_rounding=false)
 
-    if conservative_rounding
-        # Storage charging/discharging profiles
-        p_stor_charge = ceil.(Int,value.(m[:p_stor_charge]))
-        p_stor_discharge = floor.(Int,value.(m[:p_stor_discharge]))
-        e_stor = floor.(Int, value.(m[:e_stor]))
-        e_stor_ini = floor.(Int, value.(m[:stor_initial_soc]))
+    N = m[:N]
+    Nstors = m[:Nstors]
+    Ngenstors = m[:Ngenstors]
+    Ndrs = m[:Ndrs]
 
-        # Generator-Storage charging/discharging profiles
-        p_genstor_charge = ceil.(Int,value.(m[:p_genstor_charge]))
-        p_genstor_discharge = floor.(Int,value.(m[:p_genstor_discharge]))
-        e_genstor = floor.(Int,value.(m[:e_genstor]))
-        e_genstor_ini = floor.(Int, value.(m[:genstor_initial_soc]))
+    p_stor_charge = zeros(Int, Nstors, N)
+    p_stor_discharge = zeros(Int, Nstors, N)
+    e_stor = zeros(Int, Nstors, N)
+    e_stor_ini = zeros(Int, Nstors)
+    p_genstor_charge = zeros(Int, Ngenstors, N)
+    p_genstor_discharge = zeros(Int, Ngenstors, N)
+    e_genstor = zeros(Int, Ngenstors, N)
+    e_genstor_ini = zeros(Int, Ngenstors)
 
-    else
-        # Storage charging/discharging profiles
+    p_borrow_drs = zeros(Int, Ndrs, N)
+    p_payback_drs = zeros(Int, Ndrs, N)
+
+    if Nstors > 0
+            # Storage charging/discharging profiles
         p_stor_charge = round.(Int,value.(m[:p_stor_charge]))
         p_stor_discharge = round.(Int,value.(m[:p_stor_discharge]))
         e_stor = round.(Int, value.(m[:e_stor]))
         e_stor_ini = round.(Int, value.(m[:stor_initial_soc]))
+    end
 
+    if Ngenstors > 0
         # Generator-Storage charging/discharging profiles
         p_genstor_charge = round.(Int,value.(m[:p_genstor_charge]))
         p_genstor_discharge = round.(Int,value.(m[:p_genstor_discharge]))
         e_genstor = round.(Int,value.(m[:e_genstor]))
         e_genstor_ini = round.(Int, value.(m[:genstor_initial_soc]))
     end
+
+    if Ndrs > 0
+        # Demand response borrowing/payback profiles
+        p_borrow_drs = round.(Int,value.(m[:p_borrow_drs]))
+        p_payback_drs = round.(Int,value.(m[:p_payback_drs]))
+    end
+
 
     return (stor_charging=p_stor_charge,
         stor_discharging=p_stor_discharge,
@@ -34,7 +47,9 @@ function get_results(m; conservative_rounding=false)
         genstor_charging=p_genstor_charge,
         genstor_discharging=p_genstor_discharge,
         genstor_energy=e_genstor,
-        genstor_energy_initial=e_genstor_ini
+        genstor_energy_initial=e_genstor_ini,
+        drs_borrowing=p_borrow_drs,
+        drs_payback=p_payback_drs
     )
 
 end
