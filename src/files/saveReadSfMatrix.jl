8d124554e99200b1d24716a25b80a26c008907ec
Tim Kopka <tim.kopka@student.unimelb.edu.au>
2026-04-09 21:34:12 +1000

    Create path if not existing

--- src/files/saveReadSfMatrix.jl
+++ src/files/saveReadSfMatrix.jl
@@ -16,6 +16,10 @@ function saveSfMatrix(SfMatrix, filename)
     K = vcat(dims[3], K)
     V = vcat(0, V) # Add a dummy value for the dimensions entry
 
+    if !ispath(filename)
+        mkdir(dirname(filename))
+    end
+
     DataFrames.DataFrame(I=I, J=J, K=K, V=V) |> CSV.write(filename)
 end
 """
